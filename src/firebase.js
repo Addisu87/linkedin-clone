cf95bef42e8edffdb57730867880eda86c54194d
Addisu87 <addisuhaile87@gmail.com>
2021-10-13 19:25:52 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBVi0pMS1IntNLMFHO5q4iXRSYIo9Qhnbg',
+	authDomain: 'linkedin-clone-5fd63.firebaseapp.com',
+	projectId: 'linkedin-clone-5fd63',
+	storageBucket: 'linkedin-clone-5fd63.appspot.com',
+	messagingSenderId: '1049745741086',
+	appId: '1:1049745741086:web:15f304c5e9e416bb70650f',
+	measurementId: 'G-4RJHQCCRW7'
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
